3fad562b46aa0c430cf8af922c17def0e6d6c00f
xddeath69 <85794863+xddeath69@users.noreply.github.com>
2023-12-23 05:56:06 -0300

    ab

--- device.mk
+++ device.mk
@@ -6,3 +6,22 @@
 #
 
 LOCAL_PATH := device/samsung/a34x
+# A/B
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    system \
+    product \
+    vendor \
+    odm \
+    odm_dlkm \
+    vbmeta \
+    vendor_boot \
+    vendor_dlkm \
+    vbmeta_system \
+    vbmeta_vendor
+
+	
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
